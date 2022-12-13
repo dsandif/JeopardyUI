3bc13b3f47f4d297ad55d3bc331ed26b75f35a01
Darien Sandifer <dariens@duck.com>
2022-12-13 14:48:12 -0600

    Initial Commit

---
+++ Jeopardy/JeopardyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JeopardyApp.swift
+//  Jeopardy
+//
+//  Created by Darien Sandifer on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct JeopardyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
